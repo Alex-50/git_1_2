cd3a4925b2987b80126c882ffe7ad7c70ad9184e
Alexander <aleks.raz50@gmail.com>
2020-11-25 16:43:13 +0300

    It works!

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
 import sys
+from random import randint
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+from PyQt5.QtGui import QPainter, QColor
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
-class Window(QMainWindow):
+class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('main1.ui', self)
+        uic.loadUi('Ui.ui', self)
+        self.setFixedSize(600, 600)
+        self.do_paint = False
+        self.button.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            self.x = randint(100, 500)
+            self.y = randint(100, 500)
+            self.r = randint(1, 90)
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_circle(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw_circle(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        qp.drawEllipse(self.x - self.r, self.y - self.r, self.r, self.r)
+        print(self.x, self.y, self.r)
 
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = Window()
-    ex.show()
-    sys.exit(app.exec_())
+app = QApplication(sys.argv)
+ex = MyWidget()
+ex.show()
+sys.exit(app.exec_())
